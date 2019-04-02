8d96596323606634e29a78b14f0f0aa3d747e3c9
Tobias.Rimmel <tobias.rimmel@ucastme.de>
2019-04-02 11:43:28 +0200

    Set Windows Path for Linux path

--- signing.sh
+++ signing.sh
@@ -11,7 +11,7 @@ do
   if [ "$(ls -A /mnt/c/test/new_contracts)" ]; then
      if [ $fz -gt 1 ]; then
 
-      /mnt/c/Program\ Files\ \(x86\)/AutoIt3/AutoIt3.exe c:\\\test\\\signing.au3 & "/mnt/c/Program Files (x86)/digiSeal/digiSeal.exe" -sign -in c:\\\test\\\new_contracts\\\" & wait $!
+      /mnt/c/Program\ Files\ \(x86\)/AutoIt3/AutoIt3.exe c:\\\test\\\signing.au3 & /mnt/c/Program\ Files\ \(x86\)/digiSeal/digiSeal.exe -sign -in c:\\\test\\\new_contracts\\\ & wait $!
       rsync -av /mnt/c/test/new_contracts/*_sig.pdf deploy-ucastme_p@c42.ucastme.makandra.de:/gluster/shared/ucastme.de/shared/storage/ucm_contracts_signed/
       mv /mnt/c/test/new_contracts/*_sig.pdf /mnt/c/test/signed_contracts
       mv /mnt/c/test/new_contracts/*.pdf /mnt/c/test/unsigned_contracts
